bde8a2ec963e3837ad460a746097be81bda23b16
carlos.paredes <carlos.paredes@IBLAESBARC00213>
2022-01-31 09:18:38 +0100

    primer commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Formulario = () => {
+    return ( 
+        <div
+        className = "bg-white px-5 py-10 rounded-md mt-10 shadow-md md:3/4 mx-auto"
+        >
+                <h1
+                className="font-black text-xl text-gray-600"
+                >Agregar cliente</h1>
+
+        </div>
+     );
+}
+ 
+export default Formulario;
---
+++ src/paginas/EditarCliente.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EditarCliente = () => {
+  return (  
+    <>
+      <h1>Editar Cliente</h1>
+    </>
+
+  );
+}
+ 
+export default EditarCliente;
